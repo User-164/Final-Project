477e19e84a0bafab52c9ee88d6fc7c475f320a35
User-164 <lukliu990@gmail.com>
2024-06-27 14:49:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!');
-app.listen(port, () => {
-  console.log('Server is running at http://localhost:${port}');
-});
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')); }); 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
